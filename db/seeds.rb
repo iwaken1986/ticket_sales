e52a8bab1a3ea6b49a408f02d2d0b8adbeb34693
M K <mk@test.com>
2015-12-04 00:49:31 +0000

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Event.delete_all
 Event.create!([
   {id: 1, date: "2015-12-01 06:00:00", name: "ガンバ大阪杯", category: "音楽", ticket_count: 1},
   {id: 2, date: "2015-12-10 08:30:00", name: "行列のできる音楽会", category: "クラシックイベント・アート", ticket_count: 5},
@@ -12,8 +13,13 @@ Event.create!([
   {id: 5, date: "2015-12-02 10:00:00", name: "KONOYO NO OWARI ライブ", category: "音楽", ticket_count: 3},
   {id: 6, date: "2015-12-02 02:00:00", name: "猫と犬", category: "音楽", ticket_count: 8}
 ])
+
+Reservation.delete_all
 Reservation.create!([
   {event_id: 1, name: "後藤 七世", tel: "090-4384-4452"},
   {event_id: 1, name: "福岡 太郎", tel: "080-4568-1234"},
   {event_id: 1, name: "サガ ントス", tel: "090-6547-5473"}
 ])
+
+AdminUser.delete_all
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
